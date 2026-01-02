c5dcc7b56156c80577189b5a92541b4aa170eac6
Nazanin-Ahmadpour <48641349+nazanin-199@users.noreply.github.com>
2026-01-02 07:56:14 +0330

    Create __init__.py

---
+++ config/__init__.py
@@ -0,0 +1,13 @@
+from ifluid.config.config import (
+    IFLUIDConfig,
+    EmbeddingConfig,
+    GNNConfig,
+    PolicyConfig,
+)
+
+__all__ = [
+    'IFLUIDConfig',
+    'EmbeddingConfig',
+    'GNNConfig',
+    'PolicyConfig',
+]
