e21787aeec90035db6bfefd86787b6e9738a3008
Nazanin-Ahmadpour <48641349+nazanin-199@users.noreply.github.com>
2026-01-02 08:33:55 +0330

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,6 @@
 
-from ifluid.utils.logger import IFLUIDLogger
-from ifluid.utils.validators import Validators, ValidationError
+from utils.logger import IFLUIDLogger
+from utils.validators import Validators, ValidationError
 
 __all__ = [
     'IFLUIDLogger',
